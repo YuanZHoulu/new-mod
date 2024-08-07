440aac15e20f39d0ef35ce6378c9beff653ef08b
YuanZHoulu <1029909272@qq.com>
2024-08-07 20:23:58 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: YuanZHoulu <1029909272@qq.com>

---
+++ src/tutorial/ModItems.java
@@ -0,0 +1,10 @@
+package tutorial;
+
+import mindustry.type.Item;
+
+public class ModItems {
+    public static Item 赤铁矿;
+    public void load(){
+        赤铁矿 =new Item("ChiTieKuang");
+    }
+}
--- src/tutorial/TutorialMod.java
+++ src/tutorial/TutorialMod.java
@@ -1,27 +1,25 @@
-package example;
+package tutorial;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class TutorialMod extends Mod{
+    public static ModItems modItems;
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
+    public TutorialMod(){
+        Log.info("Loaded TutorialMod constructor.");
 
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("csj");
+                BaseDialog dialog = new BaseDialog("穿山甲");
                 dialog.cont.add("我地任务完成啦").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-csj")).pad(50f).row();
+                dialog.cont.image(Core.atlas.find("tutorial-mod-csj")).pad(50f).row();
                 dialog.cont.button("没了", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
@@ -30,7 +28,9 @@ public class ExampleJavaMod extends Mod{
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        Log.info("Loading some tutorial content.");
+        modItems = new ModItems();
+        modItems.load();
     }
 
 }
