cf5e813a68d16494a030c3ab9131ee1d637c2815
YuanZHoulu <1029909272@qq.com>
2024-08-14 19:35:50 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: YuanZHoulu <1029909272@qq.com>

--- src/tutorial/ModBlocks.java
+++ src/tutorial/ModBlocks.java
@@ -6,15 +6,20 @@ import mindustry.world.blocks.defense.Wall;
 import mindustry.world.meta.BuildVisibility;
 
 public class ModBlocks {
-    public static Wall 测试wall;
+    public static Wall
+            //墙
+            测试wall;
 
     public void load() {
+        //墙
         测试wall = new Wall("测试wall") {{
             requirements(Category.defense, BuildVisibility.shown, new ItemStack[]{});
-            health = 1000;
+            health = 10;
             size = 2;
             buildCostMultiplier = 0.1f;
-            armor =100.0f;
+            armor =1000.0f;
+            insulated = true;
+            absorbLasers = true;
         }};
     }
 }
--- src/tutorial/ModItems.java
+++ src/tutorial/ModItems.java
@@ -3,9 +3,13 @@ package tutorial;
 import mindustry.type.Item;
 
 public class ModItems {
-    public static Item 赤铁矿;
+    public static Item
+            //矿石
+            赤铁矿;
 
     public void load(){
+
+        //矿石
         赤铁矿 =new Item("赤铁矿");
     }
 }
