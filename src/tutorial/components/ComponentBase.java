acd762f9491f1bbebd3f438d6579bb599b54c586
YuanZHoulu <1029909272@qq.com>
2024-08-16 17:16:53 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: YuanZHoulu <1029909272@qq.com>

--- src/tutorial/components/ComponentBase.java
+++ src/tutorial/components/ComponentBase.java
@@ -4,15 +4,5 @@ import mindustry.gen.Building;
 
 public class ComponentBase<Build extends Building> {
     public void onUpdate(Build b){
-        for (Building other : b.proximity){
-            if (other.block != b.block)
-                continue;
-            float thisH = b.health;
-            float otherH = other.health;
-            if (thisH > otherH && thisH > 1 && otherH < other.maxHealth){
-                other.health += 1;
-                b.health -= 1;
-            }
-        }
     }
 }
--- src/tutorial/components/SharingHealth.java
+++ src/tutorial/components/SharingHealth.java
@@ -1,9 +1,20 @@
 package tutorial.components;
 
+import mindustry.gen.Building;
 import tutorial.blocks.StatedWall;
 
 public class SharingHealth extends ComponentBase<StatedWall.StatedWallBuild>{
     @Override
     public void onUpdate(StatedWall.StatedWallBuild b){
+            for (Building other : b.proximity) {
+                if (other.block != b.block)
+                    continue;
+                float thisH = b.health;
+                float otherH = other.health;
+                if (thisH > otherH && thisH > 1 && otherH < other.maxHealth) {
+                    other.health += 1;
+                    b.health -= 1;
+            }
+        }
     }
 }
