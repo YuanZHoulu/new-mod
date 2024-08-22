f25c80bffff5e5ebc364182871b4be6814d00cd2
YuanZHoulu <1029909272@qq.com>
2024-08-22 13:55:16 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: YuanZHoulu <1029909272@qq.com>

--- src/tutorial/ModBlocks.java
+++ src/tutorial/ModBlocks.java
@@ -9,16 +9,29 @@ import tutorial.components.SharingHealth;
 public class ModBlocks {
     public static StatedWall
             //墙
-            测试wall;
+            A测试wall,B测试wall;
 
     public static SharingHealth sharingHealth = new SharingHealth();
 
     public void load() {
         //墙
-        测试wall = new StatedWall("测试-wall") {{
+        A测试wall = new StatedWall("A测试-wall") {{
             requirements(Category.defense, BuildVisibility.shown, new ItemStack[]{
             });
-            health = 2000;
+            health = 100;
+            insulated = true;
+            absorbLasers = true;
+            armor=10;
+            size = 1;
+            buildCostMultiplier = 0.1f;
+            stateNumber = 2;
+            update = true;
+            components.add(sharingHealth);
+        }};
+        B测试wall = new StatedWall("B测试-wall") {{
+            requirements(Category.defense, BuildVisibility.shown, new ItemStack[]{
+            });
+            health = 4000;
             insulated = true;
             absorbLasers = true;
             armor=10;
--- src/tutorial/components/SharingHealth.java
+++ src/tutorial/components/SharingHealth.java
@@ -3,18 +3,21 @@ package tutorial.components;
 import mindustry.gen.Building;
 import tutorial.blocks.StatedWall;
 
+import static tutorial.ModBlocks.A测试wall;
+import static tutorial.ModBlocks.B测试wall;
+
 public class SharingHealth extends ComponentBase<StatedWall.StatedWallBuild>{
     @Override
     public void onUpdate(StatedWall.StatedWallBuild b){
             for (Building other : b.proximity) {
-                if (other.block != b.block){
+                if (other.block != A测试wall && other.block!= B测试wall){
                     continue;
                 }
                 float thisH = b.health;
                 float otherH = other.health;
-                if (thisH > otherH && thisH > 10 && otherH < other.maxHealth) {
-                    other.health += 10;
-                    b.health -= 10;
+                if (thisH > otherH && thisH > 50 && otherH < (other.maxHealth-50)) {
+                    other.health += 50;
+                    b.health -= 50;
             }
         }
     }
