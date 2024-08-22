0c82e880b7cee9a97a2b0a8cb35f93ba01fba1a0
YuanZHoulu <1029909272@qq.com>
2024-08-22 14:13:30 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: YuanZHoulu <1029909272@qq.com>

--- src/tutorial/components/SharingHealth.java
+++ src/tutorial/components/SharingHealth.java
@@ -14,10 +14,12 @@ public class SharingHealth extends ComponentBase<StatedWall.StatedWallBuild>{
                     continue;
                 }
                 float thisH = b.health;
+                float blostHealthPct = b.health / b.maxHealth;
                 float otherH = other.health;
-                if (thisH > otherH && thisH > 50 && otherH < (other.maxHealth-50)) {
-                    other.health += 50;
-                    b.health -= 50;
+                float otherlostHealthPct = other.health / other.maxHealth;
+                if (blostHealthPct > otherlostHealthPct && thisH > 100 && otherH < (other.maxHealth-100)) {
+                    other.health += 100;
+                    b.health -= 100;
             }
         }
     }
