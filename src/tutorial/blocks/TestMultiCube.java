a6180a7da2789a3d092d4884e9e1903eb66a8a9f
YuanZHoulu <1029909272@qq.com>
2024-08-22 20:17:41 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: YuanZHoulu <1029909272@qq.com>

--- src/tutorial/blocks/TestMultiCube.java
+++ src/tutorial/blocks/TestMultiCube.java
@@ -1,15 +1,23 @@
 package tutorial.blocks;
 
 import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.TextureRegion;
 import arc.math.Mathf;
+import arc.util.Eachable;
 import arc.util.Tmp;
+import mindustry.entities.units.BuildPlan;
+import mindustry.gen.Building;
 import mindustry.graphics.Drawf;
 import mindustry.graphics.Pal;
 import mindustry.world.Block;
+
 import static mindustry.Vars.*;
 import static mindustry.Vars.tilesize;
 
 public class TestMultiCube extends Block {
+    public @Load("@-top") TextureRegion topRegion;
+
     public Color baseColor = Pal.accent;
     public int range = 14;
 
@@ -26,12 +34,32 @@ public class TestMultiCube extends Block {
 
         x *= tilesize;
         y *= tilesize;
-        x += range * tilesize;
-        y += range * tilesize;
+        x += offset;
+        y += offset;
 
         Drawf.dashSquare(baseColor, x, y, range * tilesize);
         indexer.eachBlock(player.team(), Tmp.r1.setCentered(x, y, range * tilesize), b -> true, t -> {
             Drawf.selected(t, Tmp.c1.set(baseColor).a(Mathf.absin(4f, 1f)));
         });
+    }
+
+    @Override
+    public void drawPlanRegion(BuildPlan plan, Eachable<BuildPlan> list){
+        Draw.rect(region, plan.drawx(), plan.drawy());
+        Draw.rect(topRegion, plan.drawx(), plan.drawy(), plan.rotation * 90);
+    }
+
+    @Override
+    public TextureRegion[] icons(){
+        return new TextureRegion[]{region, topRegion};
+    }
+
+    public class TestMultiCubeBuild extends Building {
+
+        @Override
+        public void draw(){
+            Draw.rect(block.region, x, y);
+            Draw.rect(topRegion, x, y, rotdeg());
+        }
     }
 }
