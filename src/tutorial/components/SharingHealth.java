b9f9e064d2487511cdd13cf82c624d9875a13779
YuanZHoulu <1029909272@qq.com>
2024-08-24 13:20:48 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: YuanZHoulu <1029909272@qq.com>

--- src/tutorial/components/SharingHealth.java
+++ src/tutorial/components/SharingHealth.java
@@ -3,12 +3,18 @@ package tutorial.components;
 import mindustry.gen.Building;
 import mindustry.world.Block;
 import tutorial.blocks.StatedWall;
+
+import static mindustry.content.Blocks.copperWall;
+import static mindustry.content.Blocks.copperWallLarge;
+import static tutorial.ModBlocks.A测试wall;
+import static tutorial.ModBlocks.B测试wall;
+
 public class SharingHealth extends ComponentBase<StatedWall.StatedWallBuild> {
 
     @Override
     public void onUpdate(StatedWall.StatedWallBuild b) {
         for (Building other : b.proximity) {
-            if (b.Sharingdetect(other.block)) {
+            if (other.block == A测试wall || other.block == B测试wall || other.block == copperWall || other.block == copperWallLarge) {
                 float thisH = b.health;
                 float blostHealthPct = b.health / b.maxHealth;
                 float otherH = other.health;
