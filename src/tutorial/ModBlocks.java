b330f56f3f747b0ee60d5a2cb76a9312560f910f
YuanZHoulu <1029909272@qq.com>
2024-08-24 14:03:15 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: YuanZHoulu <1029909272@qq.com>

--- src/tutorial/ModBlocks.java
+++ src/tutorial/ModBlocks.java
@@ -33,10 +33,10 @@ public class ModBlocks {
             armor=10;
             size = 2;
             buildCostMultiplier = 0.1f;
-            stateNumber = 2;
+            stateNumber = 1;
             update = true;
             components.add(sharingHealth);
-            Availableblocks = new Block[]{A测试wall,B测试wall,copperWall,copperWallLarge};
+            Availableblocks = new Block[]{A测试wall};
         }};
         B测试wall = new StatedWall("B测试-wall") {{
             requirements(Category.defense, BuildVisibility.shown, new ItemStack[]{
@@ -50,7 +50,7 @@ public class ModBlocks {
             stateNumber = 2;
             update = true;
             components.add(sharingHealth);
-            Availableblocks = new Block[]{A测试wall,B测试wall,copperWall,copperWallLarge};
+            Availableblocks = new Block[]{B测试wall};
         }};
 
         //多方块结构
