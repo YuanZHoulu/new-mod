b974ebe5c2395d042d1e52a2d287d89e190b6551
YuanZHoulu <1029909272@qq.com>
2024-08-25 20:14:48 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: YuanZHoulu <1029909272@qq.com>

--- src/tutorial/blocks/TestMultiCube.java
+++ src/tutorial/blocks/TestMultiCube.java
@@ -68,7 +68,7 @@ public class TestMultiCube extends Wall {
         Rect rect = getRect(Tmp.r1, x, y, rotation);
 
         Drawf.dashRect(valid ? Pal.accent : Pal.remove, rect);
-        indexer.eachBlock(player.team(), Tmp.r1.setCentered(x, y, range * tilesize), b -> true, t -> {
+        indexer.eachBlock(player.team(), Tmp.r1.setCentered(rect.x, rect.y, range * tilesize), b -> true, t -> {
             Drawf.selected(t, Tmp.c1.set(baseColor).a(Mathf.absin(4f, 1f)));
         });
     }
