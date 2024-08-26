4448df647df101281a54f533fdc5c68ce045f5ee
YuanZHoulu <1029909272@qq.com>
2024-08-26 20:10:19 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: YuanZHoulu <1029909272@qq.com>

--- src/tutorial/blocks/TestMultiCube.java
+++ src/tutorial/blocks/TestMultiCube.java
@@ -1,24 +1,25 @@
 package tutorial.blocks;
 
 import arc.Core;
+import arc.Input;
 import arc.graphics.Color;
 import arc.graphics.g2d.*;
 import arc.math.Mathf;
 import arc.math.geom.Geometry;
 import arc.math.geom.Rect;
 import arc.math.geom.Vec2;
-import arc.scene.ui.layout.Scl;
-import arc.struct.Seq;
-import arc.util.Align;
+import arc.scene.ui.TextArea;
+import arc.scene.ui.layout.Table;
 import arc.util.Eachable;
 import arc.util.Tmp;
-import arc.util.pooling.Pools;
 import mindustry.entities.units.BuildPlan;
 import mindustry.gen.Building;
+import mindustry.gen.Icon;
 import mindustry.graphics.Drawf;
 import mindustry.graphics.Layer;
 import mindustry.graphics.Pal;
-import mindustry.ui.Fonts;
+import mindustry.ui.Styles;
+import mindustry.ui.dialogs.BaseDialog;
 import mindustry.world.Block;
 import mindustry.world.draw.DrawBlock;
 import mindustry.world.draw.DrawDefault;
@@ -32,6 +33,7 @@ public class TestMultiCube extends Block {
     public TextureRegion topRegion;
 
     public int range = 14;
+    public int maxNewlines = 24;
 
     public DrawBlock drawer = new DrawDefault();
 
@@ -115,30 +117,51 @@ public class TestMultiCube extends Block {
             return Tmp.v4.set(unitX, unitY);
         }
 
-        public void drawSelect(){
-            if(renderer.pixelator.enabled()) return;
-
-            Font font = Fonts.outline;
-            GlyphLayout l = Pools.obtain(GlyphLayout.class, GlyphLayout::new);
-            boolean ints = font.usesIntegerPositions();
-            font.getData().setScale(1 / 4f / Scl.scl(1f));
-            font.setUseIntegerPositions(false);
-
-            CharSequence text = message == null || message.length() == 0 ? "[lightgray]" + Core.atlas.find("tutorial-mod-zh") : message;
-
-            l.setText(font, text, Color.white, 90f, Align.left, true);
-            float offset = 1f;
-
-            Draw.color(0f, 0f, 0f, 0.2f);
-            Fill.rect(x, y - tilesize/2f - l.height/2f - offset, l.width + offset*2f, l.height + offset*2f);
-            Draw.color();
-            font.setColor(Color.white);
-            font.draw(text, x - l.width/2f, y - tilesize/2f - offset, 90f, Align.left, true);
-            font.setUseIntegerPositions(ints);
-
-            font.getData().setScale(1f);
-
-            Pools.free(l);
+        @Override
+        public void buildConfiguration(Table table) {
+            table.button(Icon.pencil, Styles.cleari, () -> {
+                if (mobile) {
+                    Core.input.getTextInput(new Input.TextInput() {{
+                        text = message.toString();
+                        multiline = true;
+                        maxLength = maxTextLength;
+                        accepted = str -> {
+                            if (!str.equals(text)) configure(str);
+                        };
+                    }});
+                } else {
+                    BaseDialog dialog = new BaseDialog("@editmessage");
+                    dialog.setFillParent(false);
+                    TextArea a = dialog.cont.add(new TextArea(message.toString().replace("\r", "\n"))).size(380f, 160f).get();
+                    a.setFilter((textField, c) -> {
+                        if (c == '\n') {
+                            int count = 0;
+                            for (int i = 0; i < textField.getText().length(); i++) {
+                                if (textField.getText().charAt(i) == '\n') {
+                                    count++;
+                                }
+                            }
+                            return count < maxNewlines;
+                        }
+                        return true;
+                    });
+                    a.setMaxLength(maxTextLength);
+                    dialog.cont.row();
+                    dialog.cont.label(() -> a.getText().length() + " / " + maxTextLength).color(Color.lightGray);
+                    dialog.buttons.button("@ok", () -> {
+                        if (!a.getText().equals(message.toString())) configure(a.getText());
+                        dialog.hide();
+                    }).size(130f, 60f);
+                    dialog.update(() -> {
+                        if (tile.build != this) {
+                            dialog.hide();
+                        }
+                    });
+                    dialog.closeOnBack();
+                    dialog.show();
+                }
+                deselect();
+            }).size(40f);
         }
     }
 }
